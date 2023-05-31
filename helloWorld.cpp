9cd2f1cba6e5f119af6aebd401b2c93741bb31db
Awais <airshadahm@gmail.com>
2023-05-30 23:21:43 -0400

    helloWorld added

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    std::cout <<"Hello World1"<<std::endl;
+    int num1, num2;
+    std::cin >>num1 >> num2;
+    std::cout <<"my numbers are:"<<num1<<" and "<<num2<<"."<<std::endl;
+    return 1;
+}
